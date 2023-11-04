65fcaa9cf864e1fc14325471b4b6f8849cef09d4
Tim <halltim@pdx.edu>
2023-11-04 15:40:15 -0700

    Created basic frame

---
+++ src/components/frame.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react"
+
+export default function Frame(props: {children: ReactNode, title: string}){
+    return (
+        <div className="bg-light container px-0 border border-dark border-3">
+            <h3 className="bg-secondary text-white text-start border-bottom border-dark border-2 ps-3 py-1">{props.title}</h3>
+            <div className="m-3">
+                {props.children}
+            </div>
+        </div>
+    )
+}
