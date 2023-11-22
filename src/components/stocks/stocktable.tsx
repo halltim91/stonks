cb8232104d3297e7ac7ccffe4cc84705ea7e47b1
Timothy hall <59980086+halltim91@users.noreply.github.com>
2023-11-21 16:45:28 -0800

    Storing progress

--- src/components/stocks/stocktable.tsx
+++ src/components/stocks/stocktable.tsx
@@ -1,7 +1,9 @@
 import './stocktable.css';
-import { useEffect, useState } from 'react';
+import { ReactNode, useEffect, useState, ReactElement} from 'react';
 import axios from 'axios';
 import Frame from '../frame';
+import StockModal from './stockModal';
+import { idText } from 'typescript';
 
 const URL: string =
   'https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=UPW9PUE4R389WR34';
@@ -18,7 +20,20 @@ interface StockData {
 // Type Property should only be either GAINERS or LOSERS consts
 export function StockTable(props: { type: string, title: string }) {
   const [data, setData] = useState([]);
+  const [modal, setModal] = useState<ReactElement>();
+  //const [stockList, setStockList] = useState([] as ReactNode[]);
+  const [stockList, setStockList] = useState([] as ReactNode[]);
+
+  function showModal(symbol: string){
+    console.log("Clicked", symbol);
+   //setStockList(stockList.concat(<p>slkfjsdlkfjsdlkfjsdlkfjsdlkfj</p>));
+    //setStockList(stockList.concat(<StockModal symbol={symbol} key="7897"/>));
+    setModal(<StockModal symbol={symbol}/>);
+    return true;
+  }
+
   useEffect(() => {
+    console.log("blah");
     axios
       .get(URL)
       .then((resp) => resp.data)
@@ -35,17 +50,20 @@ export function StockTable(props: { type: string, title: string }) {
       });
   }, []);
 
-  let stockList = [];
-  if (data !== undefined && data.length > 0) {
-    for (let i = 0; i < data.length; i++) {
-      stockList.push(<StockRow stock={data[i]} key={i} />);
+  if(stockList.length === 1) stockList.pop(); // remove the error message
+  if(stockList.length < 1){
+    if (data !== undefined && data.length > 0) {
+      for (let i = 0; i < data.length; i++) {
+        const { ticker } = data[i];
+        stockList.push(<StockRow stock={data[i]} key={i} onClick={() => showModal(ticker)}/>);
+      }
+    } else {
+      stockList.push(
+        <p className='text-danger' key='1'>
+          Request limit exceeded or Market is closed
+        </p>
+      );
     }
-  } else {
-    stockList.push(
-      <p className='text-danger' key='1'>
-        Request limit exceeded or Market is closed
-      </p>
-    );
   }
 
   return (
@@ -55,14 +73,16 @@ export function StockTable(props: { type: string, title: string }) {
         <div className=' stock-table'>
           {stockList}
         </div>
+        {modal}
       </div>
     </Frame>
   );
 }
 
-function StockRow(props: { stock: StockData }) {
+function StockRow(props: { stock: StockData, onClick: ()=>{}}) {
+  // const fcn = useCallback(() => onClick(props.stock.ticker),[onClick, props.stock.ticker]);
   return (
-    <div className='row w-100 stock-row'>
+    <div className='row w-100 stock-row' onClick={props.onClick}>
       <p className='col'>{props.stock.ticker}</p>
       <p className='col'>${formatNumber(props.stock.price, 3)}</p>
       <p className='col'>${formatNumber(props.stock.change_amount, 3)}</p>
@@ -80,6 +100,10 @@ function HeaderRow() {
       <p className='col fw-bold'>% Chg</p>
     </div>
   );
+}
+
+function createModal(symbol: string){
+  return (<StockModal symbol={symbol}/>)
 }
 
 /**Trims number string to only have 2 characters after decimal (X.XX) */
