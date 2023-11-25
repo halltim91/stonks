086d4652b6b371f3e9a0c773803ed46b473d69d2
shrutivd <svd@pdx.edu>
2023-11-25 09:41:49 -0800

    added intraday api and candlestick chart

--- src/components/forex/Intraday.tsx
+++ src/components/forex/Intraday.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import ApiReq, { apiReqData, preReq } from './apiReq';
 import axios from 'axios';
-import { ReactNode } from 'react';
+import Plot from 'react-plotly.js';
+import ApiReq, { apiReqData, preReq } from './apiReq';
 
 export interface IntradayInfo {
   'Meta Data': {
@@ -24,22 +24,50 @@ const Intraday: React.FC<{ toCurrency: string | undefined }> = (props) => {
 
   useEffect(() => {
     const fetchData = async () => {
-      console.log("to currency :", props.toCurrency);
-
       const url = `https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol=USD&to_symbol=${props.toCurrency}&interval=5min&apikey=${apiReqData.apiKey}`;
-      console.log("url :", url);
       
       const response = await axios.get(url);
 
       setIntradayData(response.data);
     };
     fetchData();
-  }, []);
+  }, [props.toCurrency]);
 
-  return (<div>
-    <h1>Intraday</h1>
-    <p>{intradayData?.['Meta Data']['1. Information']}</p>
-    </div>)
+  const chartData = Object.entries(intradayData?.['Time Series FX (5min)'] || {}).map(
+    ([time, values]) => ({
+      x: new Date(time),
+      open: parseFloat(values['1. open']),
+      high: parseFloat(values['2. high']),
+      low: parseFloat(values['3. low']),
+      close: parseFloat(values['4. close']),
+    })
+  );
+
+  const layout = {
+    title: `Intraday Chart (${props.toCurrency})`,
+    xaxis: {
+      type: 'date' as const,
+      title: 'Time',
+    },
+    yaxis: {
+      title: 'Price',
+    },
+  };
+
+  return (
+    <div>
+      <h1>Intraday</h1>
+      <p>{intradayData?.['Meta Data']['1. Information']}</p>
+      <Plot data={[{
+        type: 'candlestick',
+        x: chartData.map(data => data.x),
+        open: chartData.map(data => data.open),
+        high: chartData.map(data => data.high),
+        low: chartData.map(data => data.low),
+        close: chartData.map(data => data.close),
+      }]} layout={layout} />
+    </div>
+  );
 };
 
 export default Intraday;
